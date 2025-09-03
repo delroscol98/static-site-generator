43d8b30a189001d62a9f68599badfe582fe4f815
delroscol98 <collindelrosario98@gmail.com>
2025-09-03 21:30:26 +1000

    feature: added function to change TextNode to LeafNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,19 +1,12 @@
 from enum import Enum
 
+from leafnode import LeafNode
 
-class TextType(Enum):
-    TEXT = "text"
-    BOLD = "bold"
-    ITALIC = "italic"
-    CODE = "code"
-    LINK = "link"
-    IMAGE = "image"
+TextType = Enum("TextType", ["TEXT", "BOLD", "ITALIC", "CODE", "LINK", "IMAGE"])
 
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
-        if not isinstance(text_type, TextType):
-            raise TypeError("text_type must be a TextType member")
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -23,3 +16,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    text_type = text_node.text_type
+    text = text_node.text
+    url = text_node.url
+    if text_type == TextType.TEXT:
+        return LeafNode(None, text)
+    if text_type == TextType.BOLD:
+        return LeafNode("b", text)
+    if text_type == TextType.ITALIC:
+        return LeafNode("i", text)
+    if text_type == TextType.CODE:
+        return LeafNode("code", text)
+    if text_type == TextType.LINK:
+        return LeafNode("a", text, {"href": url})
+    if text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": url, "alt": text})
+    raise TypeError("text_type must be a TextType member")
