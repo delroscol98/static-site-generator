481cb83f9fdebe9b2b8e497498790df2f947f295
delroscol98 <collindelrosario98@gmail.com>
2025-09-03 18:17:01 +1000

    feature: added LeafNode to represent DOM nodes without children

---
+++ src/leafnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag, props=None):
+        super().__init__(value, tag, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value of leaf node cannot be none")
+        if self.tag is None:
+            return self.value
+
+        if self.props is not None:
+            attributes = super().props_to_html()
+
+            return f"<{self.tag} {attributes}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
