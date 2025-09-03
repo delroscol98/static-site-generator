35a6ad2c98858cf9e71ac1b8d0216e3df932becc
delroscol98 <collindelrosario98@gmail.com>
2025-09-03 19:49:54 +1000

    test: tests updated

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -36,5 +36,28 @@ class TestParentNode(unittest.TestCase):
             "<div><span><b>grandchild</b></span></div>",
         )
 
+    def test_with_children_props(self):
+        p = LeafNode("p", "This is a paragraph tag", {"id": "text__main"})
+        a = LeafNode("a", "Click me", {"href": "https://www.example.com"})
+        div = ParentNode("div", [p, a])
+        self.assertEqual(
+            div.to_html(),
+            '<div><p id="text__main">This is a paragraph tag</p><a href="https://www.example.com">Click me</a></div>',
+        )
+
+    def test_with_grandchildren_props(self):
+        h1 = LeafNode("h1", "This is a heading", {"id": "heading-left"})
+        p = LeafNode("p", "This is a paragraph", {"id": "paragraph-left"})
+        h2 = LeafNode("h2", "This is another heading")
+
+        article = ParentNode("article", [h1, p])
+        section = ParentNode("section", [article, h2])
+
+        self.assertEqual(
+            section.to_html(),
+            '<section><article><h1 id="heading-left">This is a heading</h1><p id="paragraph-left">This is a paragraph</p></article><h2>This is another heading</h2></section>',
+        )
+
+
 if __name__ == "__name__":
     unittest.main()
