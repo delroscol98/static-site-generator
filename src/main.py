ecf9844e3a8f2724b216e63b7a505f823c1767f5
delroscol98 <collindelrosario98@gmail.com>
2025-09-03 18:14:01 +1000

    feature: created TextNode to handle inline text objects

---
+++ src/main.py
@@ -0,0 +1,16 @@
+
+from textnode import TextNode, TextType
+
+
+def main():
+    print('Hello world!')
+
+    dummy2 = TextNode(
+            "This is some anchor text",
+            TextType.LINK,
+            "https://www.boot.dev"
+    )
+
+    print(dummy2)
+
+main()
