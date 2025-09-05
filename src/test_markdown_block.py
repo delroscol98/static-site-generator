c2ab13414beb9f4960d57292d63e5f1c142b3e97
delroscol98 <collindelrosario98@gmail.com>
2025-09-05 13:31:04 +1000

    test: added tests

--- src/test_markdown_block.py
+++ src/test_markdown_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_block import markdown_to_blocks
+from markdown_block import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkDownBlock(unittest.TestCase):
@@ -47,6 +47,55 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_heading(self):
+        block1 = "#This is a heading"
+        block2 = "##This is a heading"
+        block_type1 = block_to_block_type(block1)
+        block_type2 = block_to_block_type(block2)
+
+        self.assertEqual(block_type1, BlockType.HEADING)
+        self.assertEqual(block_type2, BlockType.HEADING)
+
+    def test_block_to_code(self):
+        block = """```
+            console.log("This is JavaScript in Python")
+        ```"""
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_quote(self):
+        block = "> With great power comes great responsibility"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_unordered_single(self):
+        block = "- There is one item"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_unordered_multiple(self):
+        block = "- This is one item\n- This is another item\n- This is the final item"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_ordered_single(self):
+        block = "1. There is one item"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_block_to_ordered_multiple(self):
+        block = (
+            "1. This is one item\n2. This is another item\n3. This is the final item"
+        )
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
 
 
 if __name__ == "__main__":
